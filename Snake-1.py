739ca0e4839c054ea89d855dcf70ad2e63ff506e
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-09-19 17:16:07 -0500

    Tablero ver 1

--- Snake-1.py
+++ Snake-1.py
@@ -1,1 +1,19 @@
-print("Carlos :)")
+from os import system
+#Descomentar si se va a usar colab
+#from google.colab import output
+
+tablero = []
+for k in range(0, 13):
+  tablero.append(["·" for i in range(0,13)])  
+while True:
+  for j in range(0, 13):
+    for i in range(0, 13):
+      print(tablero[j][i], end=" ")
+    print()
+
+
+  if input() == "y":
+    break  
+#Descomentar si se va a usar colab
+  #output.clear()
+  system("cls")
