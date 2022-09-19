e07b7c69b2c43d1cd71696c86629f6b22540847c
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-09-19 18:48:43 -0500

    Manzana y cabeza

--- Snake-1.py
+++ Snake-1.py
@@ -1,19 +1,65 @@
 from os import system
+import movimiento as mov
+from random import randrange
+
 #Descomentar si se va a usar colab
 #from google.colab import output
 
+"""
+NOTA
+
+1 : Es la cabeza de la serpiente
+0 : Es el cuerpo de la serpiente
+@ : Es la manzana
+"""
+
+#Creación del tablero
 tablero = []
 for k in range(0, 13):
-  tablero.append(["·" for i in range(0,13)])  
+    tablero.append(["·" for i in range(0,13)])  
+    
+#Creación de la serpiente
+cabeza = [randrange(13), randrange(13)]
+tablero[cabeza[0]][cabeza[1]] = "1"
+
+#Función para hacer aparecer la mazana en otra posición
+def manzana_posi():
+    global manzana
+    manzana = [randrange(13), randrange(13)]
+
+#Primera posición de la manzana
+manzana_posi()
+
 while True:
-  for j in range(0, 13):
-    for i in range(0, 13):
-      print(tablero[j][i], end=" ")
-    print()
 
+    #Creación de la manzana en el tablero
+    r = 0
+    while manzana == cabeza or """manzana is in cuerpo""" :
+        r += 1
+        manzana_posi()
+        print(manzana, r)
+    tablero[manzana[0]][manzana[1]] = "@"
 
-  if input() == "y":
-    break  
-#Descomentar si se va a usar colab
-  #output.clear()
-  system("cls")
+    #Este for dibuja todo
+    for j in range(0, 13):
+        for i in range(0, 13):
+            print(tablero[j][i], end=" ")
+        print()
+
+    #Movimiento 
+    move = str(input())
+    move = move.lower()
+    if move == "w":
+        mov.up(tablero)
+    elif move == "a":
+        mov.left(tablero)
+    elif move == "s":
+        mov.down(tablero)
+    elif move == "d":
+        mov.right(tablero)
+    else:
+        mov.error()
+
+    #Descomentar si se va a usar colab
+    #output.clear()
+    system("cls")
---
+++ movimiento.py
@@ -0,0 +1,14 @@
+def up(t):
+    print("Arriba")
+
+def left(t):
+    print("Izquierda")
+
+def down(t):
+    print("Abajo")
+
+def right(t):
+    print("Derecha")
+
+def error():
+    print("Error")
