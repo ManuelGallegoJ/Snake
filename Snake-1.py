f8a1f52ce99a6761e1b6686c5ca7d42ed6cfa74e
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-09-19 23:37:49 -0500

    Nueva cabeza

--- Snake-1.py
+++ Snake-1.py
@@ -5,17 +5,10 @@ from random import randrange
 #Descomentar si se va a usar colab
 #from google.colab import output
 
-
-"""►
-◄
-▲
-▼"""
-
-
 """
 NOTA
 
-1 : Es la cabeza de la serpiente
+▲ : Es la cabeza de la serpiente
 0 : Es el cuerpo de la serpiente
 @ : Es la manzana
 """
@@ -31,8 +24,10 @@ def manzana_posi():
 #Primera posición de la manzana
 manzana = [3,9]
 
+come = False
 creci = 0
-
+cont_com = 0
+move = "w"
 while True:
     if cabeza[0] == [55,55]:
         print("Has perdido")
@@ -46,7 +41,14 @@ while True:
     for j in range(0, 13):
         for i in range(0, 13):
             if j == cabeza[0][0] and i == cabeza[0][1]:
-                print("1", end=" ")
+                if move == "w":
+                    print("▲", end=" ")
+                elif move == "a":
+                    print("◄", end=" ")
+                elif move == "d":
+                    print("►", end=" ")
+                else:
+                    print("▼", end=" ")
             elif j == manzana[0] and i == manzana[1]:
                 print("@", end=" ") 
             elif [j, i] in cabeza[1:]:
@@ -72,15 +74,25 @@ while True:
         cabeza.pop(-1)
     else:
         cabeza[0] = mov.error()
-
+    cont_com += 1
     if cabeza[0] in cabeza[1:]:
         print("Has perdido")
         break
 
+    #Come manzana
     if cabeza[0] == manzana:
+        cont_com = 0
         creci +=  2
-        manzana_posi()
+        manzana = [-1,-1]
+        ran_pos = randrange(9)+1
+        come = True
 
+    if come and ran_pos == cont_com:
+        manzana_posi()
+        come = False
+        cont_com = 0
+
+    #Crecimiento
     if creci > 0:
         cabeza.append(cabeza[-1])
         creci -= 1
